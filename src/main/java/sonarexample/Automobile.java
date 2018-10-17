d04bd5644ffe2c5019bb97986917c31e3da4a4b1
Praveen Meruga <praveenmeruga@cloud.upwork.com>
2018-10-17 17:17:38 -0400

    init

--- src/main/java/sonarexample/Automobile.java
+++ src/main/java/sonarexample/Automobile.java
@@ -5,4 +5,16 @@ public class Automobile {
     public String getEngineCapacity() {
         return null;
     }
+
+    public String getPlatform() {
+        return null;
+    }
+
+    public String getChasisInformation() {
+        return null;
+    }
+
+    public String getMake() {
+        return null;
+    }
 }
